a37734535c277d8a9ffc3739795b2f67f982368f
richardrex <rfrf26@mail.ru>
2020-04-06 14:08:01 +0300

    next

--- results/assigments/assigment_1.R
+++ results/assigments/assigment_1.R
@@ -5,3 +5,13 @@ aa = function(x,y){
   x*y
   }
 results <- aa(Temperatures,WEIGHTS)
+#explorers question 2
+Rc = 185000 #catchment area in km2
+Rc
+L = 1233000 #length of Rhine
+rw = 185000 * 1000000/1233000 #rhine average width
+rw
+perc = 0.05 * 24 #precipitation in 24 h
+perc
+ro = (0.05/3600) * rw #increase in runoff
+ro
